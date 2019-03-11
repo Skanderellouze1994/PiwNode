69dad1fdfde4dc9da0ff478cd39a1db2b115eb7a
chaimaB <chaima.braiek@esprit.tn>
2019-03-11 15:14:17 +0100

    Home component

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';
 import Menu from './components/Menu';
 import {Login} from './components/Login';
 import {Signup} from './components/Signup';
+import {Home} from './components/Home';
 
 class App extends Component {
   render() {
@@ -18,6 +18,7 @@ class App extends Component {
         <Switch>
             <Route exact path="/login" component={Login}/>
             <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/home" component={Home}/>
         </Switch>
         <Footer />
         </div>
