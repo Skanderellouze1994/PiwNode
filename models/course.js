7512735bcdd7deb46d65e34d765800d663c2dec3
chaimaB <chaima.braiek@esprit.tn>
2019-04-24 17:37:58 +0100

    Add canvas js

--- models/course.js
+++ models/course.js
@@ -26,7 +26,8 @@ var courseSchema = new mongoose.Schema({
     },
     ressources: [{type:mongoose.Schema.Types.Object,ref:'Ressources'}],
     tutorCreator: {type:mongoose.Schema.Types.ObjectId,ref:'User'},
-    quiz: {type:mongoose.Schema.Types.Object,ref:'Quiz'}
+    quiz: {type:mongoose.Schema.Types.Object,ref:'Quiz'},
+    presenceList: [{type:mongoose.Schema.Types.ObjectId,ref:'User'}]
 });
 
 module.exports = mongoose.model('Course', courseSchema);
