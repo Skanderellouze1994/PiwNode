5bd239355d2f4d95b955c59a44259e4c0b0afeca
Achraf <achraf.douss@esprit.tn>
2019-05-08 05:02:11 +0100

    k

--- routes/scrapping.js
+++ routes/scrapping.js
@@ -6,7 +6,7 @@ var router = express.Router();
 const scrapedin = require('../scraping/scrapedin/scrapedin')
 //const scrapedin = require('scrapedin')
 var scrapping = async (url)=>new Promise(async (resolve,reject) => {
-    const profileScraper = await scrapedin({email: 'csmajs2019@gmail.com', password: 'piweb2019',isHeadless:false})
+    const profileScraper = await scrapedin({email: 'achraf.douss@esprit.tn', password: 'aqw123147',isHeadless:false})
     const profile = await profileScraper(url)
     resolve(profile)
     reject(profile)
